680e918f95e6f7f374bc09e682cdd9732dcf0611
hernc <herncar2@gmail.com>
2022-05-26 16:12:54 -0600

    worked on CRUD functions

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const tasks = require('./tasks.js')
+
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+app.get('/', (req, res) => {
+  tasks.getTasks()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.post('/tasks', (req, res) => {
+    const {description} = req.body
+  tasks.addTask(description)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.delete('/tasks/:id', (req, res) => {
+  tasks.deleteTask(req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+  app.put('/tasks/:id', (req, res) => {
+    
+  tasks.editTask(req.body,req.params.id)
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+  
+  
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
---
+++ tasks.js
@@ -0,0 +1,79 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database:'todo_list',
+    password:'postgres',
+    port: 5432,
+})
+
+const getTasks=()=>{
+    return new Promise( function(resolve, reject){
+        pool.query('SELECT * FROM tasks ORDER BY id ASC',(error, results)=>{
+            if(error){
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+const addTask = (description)=>{
+    return new Promise( function(resolve, reject){
+
+        pool.query('INSERT INTO tasks (description,status) VALUES($1,$2) RETURNING *',[description,'false'],(error, results)=>{
+            if(error){
+                reject(error)
+            }
+            resolve('New task has been added to ToDo list');
+        })
+    })
+
+}
+
+const deleteTask= (id)=>{
+    return new Promise( function(resolve, reject){
+    
+        pool.query('DELETE FROM tasks WHERE id = $1', [id], (error, results) => {
+            if (error) {
+              reject(error)
+            }
+            resolve(`Task Successfully Deleted`)
+          })
+        })
+
+}
+const editTask=(body, id)=>{
+    return new Promise( function(resolve, reject){
+        const {description,status}=body
+        console.log(body)
+        console.log(description)
+        console.log(status)
+        pool.query('UPDATE tasks SET description =$1,status=$2 WHERE id=$3', [description,status,parseInt(id)], (error, results) => {
+            if (error) {
+              reject(error)
+            }
+            resolve(`Task Successfully Updated`)
+          })
+        })
+
+}
+const completeTask=(id)=>{
+    return new Promise( function(resolve, reject){
+        
+        pool.query('UPDATE tasks SET status =$1 WHERE id=$2', ['True',id], (error, results) => {
+            if (error) {
+              reject(error)
+            }
+            resolve(`Task Successfully Completed`)
+          })
+        })
+
+}
+
+module.exports={
+    getTasks,
+    deleteTask,
+    addTask,
+    editTask,
+    completeTask,
+}
